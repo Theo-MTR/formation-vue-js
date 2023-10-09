b17c22d25fc35dd9ab3bbf7b61070c1d52e5f2ff
Théo <t.monteiro@waigeo.fr>
2023-10-09 12:06:35 +0200

    Exercice 5

--- 05_arrayOperations.js
+++ 05_arrayOperations.js
@@ -1,17 +1,23 @@
+const tableauChiffre = [10, 12, 16];
+const tableauMots = ["toto", "titi", "tata", "abricot", "Arbre"];
+const tableauUtilisateurs = [
+    {id: 1, name: 'John'},
+    {id: 2, name: 'Doe'},
+    {id: 3, name: 'Foo'},
+    {id: 4, name: 'Bar'},
+];
 /**
  * Utiliser la fonction .map sur le tableau passé en paramètre
  * pour retourner un nouveau tableau avec les valeurs multipliées par 2
- * 
- * contraintes: 
+ *
+ * contraintes:
  *   - Les mots clées for, while, do while sont interdits
  *   - les mots clées function et return sont interdits
  *   - Vous ne pouvez pas utiliser de variable
- * 
+ *
   */
 
-const multiplyByTwo = (array) => {};
-
-console.log(multiplyByTwo());
+const multiplyByTwo = (array) => array.map((value) => value * 2);
 /**
  * Utiliser la fonction .filter sur le tableau passé en paramètre
  * retourne un nouveau tableau avec uniquement les nom qui commencent par la lettre "A"
@@ -22,7 +28,7 @@ console.log(multiplyByTwo());
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const filterNameStartByA = (array) => {}
+const filterNameStartByA = (array) => array.filter((value) => value.startsWith('A'));
 
 /**
  * Utiliser la fonction .reduce sur le tableau passé en paramètre
@@ -34,7 +40,7 @@ const filterNameStartByA = (array) => {}
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const sum = (array) => {}
+const sum = (array) => array.reduce((a, b) => a + b);
 
 /**
  * Utiliser la fonction .find sur le tableau passé en paramètre
@@ -55,7 +61,12 @@ const sum = (array) => {}
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const findUserById = (array, id) => {}
+const findUserById = (array, id) => array.find((user) => user.id === id);
 
+
+console.log(multiplyByTwo(tableauChiffre));
+console.log(filterNameStartByA(tableauMots));
+console.log(sum(tableauChiffre));
+console.log(findUserById(tableauUtilisateurs, 3))
 
 module.exports = {multiplyByTwo, filterNameStartByA, sum, findUserById};
