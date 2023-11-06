b7fdfd6c4f9bbe8507bd3ce4b5597027b9780231
Théo <t.monteiro@waigeo.fr>
2023-11-06 10:41:49 +0100

    Exercice 10

--- 10_promise.js
+++ 10_promise.js
@@ -1,12 +1,15 @@
 
 /**
  * Créez une fonction asynchrone qui attend 2 seconde
- * 
+ *
  * utilisez new Promise
  */
 
-const sleep = () => {
+const sleep = () => new Promise((res) => setTimeout(() => {
+    res('done');
+}, 2000));
 
-};
-
+sleep().then((res) => {
+    console.log(res);
+});
 module.exports = {sleep};
